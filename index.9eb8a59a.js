79977cce67fd7ffc5bf85db48dea86046bbcc346
Andrey Bondar <andrey_bondar@outlook.com>
2023-12-06 16:36:39 +0200

    make build

---
+++ index.9eb8a59a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=getComputedStyle(e),l=parseFloat(t.height),o=parseFloat(t.width),a=document.querySelector(".wall"),p=getComputedStyle(a),r=parseFloat(p.height),c=parseFloat(p.width);e.style.top="".concat((r-l)/2,"px"),e.style.left="".concat((c-o)/2,"px");
+//# sourceMappingURL=index.9eb8a59a.js.map
